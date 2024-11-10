c3d8bb746081ae7222f231cbc56107bd3bf4cae9
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-11-10 10:10:30 +0100

    User record for the options class

--- dotnet/Common/AiDevsApi/Models/AiDevsApiOptions.cs
+++ dotnet/Common/AiDevsApi/Models/AiDevsApiOptions.cs
@@ -2,12 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Common.AiDevsApi.Models;
 
-public class AiDevsApiOptions
+public record AiDevsApiOptions
 {
     public const string SectionName = "AiDevsApi";
 
     [Required]
-    public required string ApiKey { get; set; }
+    public required string ApiKey { get; init; }
 
     [Required]
     public required string BaseUrl { get; init; }
