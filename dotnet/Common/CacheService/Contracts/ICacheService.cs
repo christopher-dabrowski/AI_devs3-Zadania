702f59896bb75193e9b8b6ba782b1c761a3e34c3
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-12-02 09:57:04 +0100

    Bring back non generic ICacheService GetAsync and SetAsync

--- dotnet/Common/CacheService/Contracts/ICacheService.cs
+++ dotnet/Common/CacheService/Contracts/ICacheService.cs
@@ -3,7 +3,9 @@ namespace Common.Cache.Contracts;
 public interface ICacheService
 {
     Task<T?> GetAsync<T>(string key);
+    Task<string?> GetAsync(string key);
     Task SetAsync<T>(string key, T data);
+    Task SetAsync(string key, string data);
     Task<bool> ExistsAsync(string key);
 
     Task<byte[]?> GetAsyncBytes(string key);
