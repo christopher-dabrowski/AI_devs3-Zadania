209cc5b1c4fc3c4ec8346fde346e1d38ed5a125a
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-12-02 20:40:48 +0100

    Search quadrant collection

--- dotnet/S03E02/GlobalUsings.cs
+++ dotnet/S03E02/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Options;
 global using OpenAI;
 global using System.Text.Json;
+global using System.Collections.Immutable;
+global using Common.AiDevsApi.Contracts;
+global using Common.AiDevsApi.Models;
--- dotnet/S03E02/Program.cs
+++ dotnet/S03E02/Program.cs
@@ -1,4 +1,3 @@
-using OpenAI.Embeddings;
 using static MoreLinq.Extensions.EquiZipExtension;
 using S03E02;
 using Qdrant.Client;
@@ -12,38 +11,73 @@ var host = builder.Build();
 await using var scope = host.Services.CreateAsyncScope();
 var sp = scope.ServiceProvider;
 
-var openAiClient = sp.GetRequiredService<OpenAIClient>();
 var embeddingModel = "text-embedding-3-small";
-var embeddingsClient = openAiClient.GetEmbeddingClient(embeddingModel);
-
-var weaponsTestFiles = await
-    Directory.EnumerateFiles("Resources/weapons_tests", "*.txt")
-    .Select(async file => (Date: Path.GetFileNameWithoutExtension(file), Description: await File.ReadAllTextAsync(file)))
-    .ToArray()
-    .ToAsyncEnumerable()
-    .SelectAwait(async x => ((await x).Date, (await x).Description))
-    .ToArrayAsync();
-
-var descriptions = weaponsTestFiles.Select(x => x.Description);
-
-var embeddings = await embeddingsClient.GenerateEmbeddingsAsync(descriptions);
-
-var weaponsTestEmbeddings = weaponsTestFiles
-    .EquiZip(embeddings.Value, (x, y) => (x.Date, x.Description, Embedding: y))
-    .ToArray();
+var collectionName = "weapons_tests";
 
 var quadrant = new QdrantClient("localhost", 6334);
+var openAiClient = sp.GetRequiredService<OpenAIClient>();
+var embeddingsClient = openAiClient.GetEmbeddingClient(embeddingModel);
 
-var collectionName = "weapons_tests";
-if (await quadrant.CollectionExistsAsync(collectionName))
-    await quadrant.DeleteCollectionAsync(collectionName);
-await quadrant.CreateCollectionAsync(collectionName, vectorsConfig: new VectorParams
+await FillVectorDatabase();
+
+var question = "W raporcie, z którego dnia znajduje się wzmianka o kradzieży prototypu broni?";
+var questionEmbedding = await embeddingsClient.GenerateEmbeddingAsync(question);
+
+var result = await quadrant.SearchAsync(collectionName, questionEmbedding.Value.ToFloats(), limit: 1);
+var foundDate = result.First().Payload["date"].StringValue;
+
+var aiDevsService = sp.GetRequiredService<IAiDevsApiService>();
+var formattedDate = foundDate.Replace("_", "-");
+Console.WriteLine(formattedDate);
+
+var taskAnswer = new TaskAnswer
 {
-    Size = EmbeddingLengthByModel[embeddingModel],
-    Distance = Distance.Cosine
-});
+    Task = "wektory",
+    Answer = formattedDate
+};
 
+var response = await aiDevsService.VerifyTaskAnswerAsync(taskAnswer);
+Console.WriteLine(JsonSerializer.Serialize(response));
 
+async Task FillVectorDatabase()
+{
+    if (await quadrant.CollectionExistsAsync(collectionName))
+        return;
+
+    var weaponsTestFiles = await
+        Directory.EnumerateFiles("Resources/weapons_tests", "*.txt")
+        .Select(async file => (Date: Path.GetFileNameWithoutExtension(file), Description: await File.ReadAllTextAsync(file)))
+        .ToArray()
+        .ToAsyncEnumerable()
+        .SelectAwait(async x => ((await x).Date, (await x).Description))
+        .ToArrayAsync();
+
+    var descriptions = weaponsTestFiles.Select(x => x.Description);
+
+    var embeddings = await embeddingsClient.GenerateEmbeddingsAsync(descriptions);
+
+    var weaponsTestEmbeddings = weaponsTestFiles
+        .EquiZip(embeddings.Value, (x, y) => (x.Date, x.Description, Embedding: y))
+        .ToArray();
+
+    await quadrant.CreateCollectionAsync(collectionName, vectorsConfig: new VectorParams
+    {
+        Size = EmbeddingLengthByModel[embeddingModel],
+        Distance = Distance.Cosine
+    });
+
+    var points = weaponsTestEmbeddings.Select((x, i) => new PointStruct
+    {
+        Id = (ulong)i,
+        Vectors = x.Embedding.ToFloats().ToArray(),
+        Payload =
+    {
+        ["date"] = x.Date,
+    }
+    }).ToImmutableArray();
+
+    await quadrant.UpsertAsync(collectionName, points);
+}
 
 partial class Program
 {
