e1ff716ee60d7f9f903e126487d4cb92a777339e
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-12-02 20:16:36 +0100

    Crete quadrant collection

--- dotnet/S03E02/Program.cs
+++ dotnet/S03E02/Program.cs
@@ -1,6 +1,8 @@
 using OpenAI.Embeddings;
 using static MoreLinq.Extensions.EquiZipExtension;
 using S03E02;
+using Qdrant.Client;
+using Qdrant.Client.Grpc;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -11,7 +13,8 @@ await using var scope = host.Services.CreateAsyncScope();
 var sp = scope.ServiceProvider;
 
 var openAiClient = sp.GetRequiredService<OpenAIClient>();
-var embeddingsClient = openAiClient.GetEmbeddingClient("text-embedding-3-small");
+var embeddingModel = "text-embedding-3-small";
+var embeddingsClient = openAiClient.GetEmbeddingClient(embeddingModel);
 
 var weaponsTestFiles = await
     Directory.EnumerateFiles("Resources/weapons_tests", "*.txt")
@@ -29,3 +32,24 @@ var weaponsTestEmbeddings = weaponsTestFiles
     .EquiZip(embeddings.Value, (x, y) => (x.Date, x.Description, Embedding: y))
     .ToArray();
 
+var quadrant = new QdrantClient("localhost", 6334);
+
+var collectionName = "weapons_tests";
+if (await quadrant.CollectionExistsAsync(collectionName))
+    await quadrant.DeleteCollectionAsync(collectionName);
+await quadrant.CreateCollectionAsync(collectionName, vectorsConfig: new VectorParams
+{
+    Size = EmbeddingLengthByModel[embeddingModel],
+    Distance = Distance.Cosine
+});
+
+
+
+partial class Program
+{
+    static IReadOnlyDictionary<string, ulong> EmbeddingLengthByModel = new Dictionary<string, ulong>
+    {
+        { "text-embedding-3-small", 1536UL },
+        { "text-embedding-3-large", 3072UL },
+    };
+}
