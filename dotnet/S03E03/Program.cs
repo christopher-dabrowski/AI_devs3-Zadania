6c713db6f96d18c49c7f8ce2b9e120d1dbb9fad3
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-12-05 20:15:18 +0100

    Prepare the tools

--- dotnet/S03E03/Program.cs
+++ dotnet/S03E03/Program.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using OpenAI.Chat;
 using S03E03;
 using S03E03.Models;
 
@@ -13,7 +14,50 @@ var host = builder.Build();
 await using var scope = host.Services.CreateAsyncScope();
 var sp = scope.ServiceProvider;
 
-// Main logic will go here 
+var showTablesTool = ChatTool.CreateFunctionTool(
+    functionName: nameof(ShowTables),
+    functionDescription: "List tables in the database"
+);
+
+var getTableSchemaTool = ChatTool.CreateFunctionTool(
+    functionName: nameof(GetTableSchema),
+    functionDescription: "Get the SQL code that was used to crate the table. You can use this to learn the table schema",
+    functionParameters: BinaryData.FromBytes("""
+        {
+            "type": "object",
+            "properties": {
+                "tableName": {
+                    "type": "string",
+                    "description": "Name of the database table"
+                }
+            },
+            "required": ["tableName"],
+            "additionalProperties": false
+        }
+        """u8.ToArray())
+);
+
+var runSqlTool = ChatTool.CreateFunctionTool(
+    functionName: nameof(RunDbCommand),
+    functionDescription: "Run a SQL command on the database",
+    functionParameters: BinaryData.FromBytes("""
+        {
+            "type": "object",
+            "properties": {
+                "command": {
+                    "type": "string",
+                    "description": "Text of the SQL command to run"
+                }
+            },
+            "required": ["command"],
+            "additionalProperties": false
+        }
+        """u8.ToArray())
+);
+
+async Task<string> ShowTables() => await RunDbCommand("show tables");
+
+async Task<string> GetTableSchema(string tableName) => await RunDbCommand($"show create table {tableName}");
 
 async Task<string> RunDbCommand(string command)
 {
@@ -33,3 +77,4 @@ async Task<string> RunDbCommand(string command)
 
     return await response.Content.ReadAsStringAsync();
 }
+
