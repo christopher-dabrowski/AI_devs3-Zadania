f2e7abe837af1004f460ac887e56326d71891e35
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2024-12-05 21:04:22 +0100

    Use the agent tool usage to solve the task :D

--- dotnet/S03E03/Program.cs
+++ dotnet/S03E03/Program.cs
@@ -6,9 +6,7 @@ using S03E03.Models;
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services
-    .AddS03E03()
-    .Configure<S03E03Options>(
-        builder.Configuration.GetSection(nameof(S03E03Options)));
+    .AddS03E03();
 
 var host = builder.Build();
 await using var scope = host.Services.CreateAsyncScope();
@@ -55,6 +53,130 @@ var runSqlTool = ChatTool.CreateFunctionTool(
         """u8.ToArray())
 );
 
+List<ChatMessage> messages =
+[
+    new SystemChatMessage(
+        """
+        You are a helpful assistant with access to tools for interacting with a SQL database.
+        Use the tools to get the correct data from the database.
+        First learn of the database structure, then query it for the relevant data.
+        
+        Whey you are ready to give the final response start it with "ANSWER:" and provide only the requred data separated by ", "
+        """),
+    new UserChatMessage("które aktywne datacenter (DC_ID) są zarządzane przez pracowników, którzy są na urlopie (is_active=0)"),
+];
+
+ChatCompletionOptions options = new()
+{
+    Tools = { showTablesTool, getTableSchemaTool, runSqlTool },
+};
+
+var client = sp.GetRequiredService<ChatClient>();
+
+bool requiresAction;
+
+do
+{
+    requiresAction = false;
+    ChatCompletion completion = await client.CompleteChatAsync(messages, options);
+
+    switch (completion.FinishReason)
+    {
+        case ChatFinishReason.Stop:
+            {
+                messages.Add(new AssistantChatMessage(completion));
+                break;
+            }
+        case ChatFinishReason.ToolCalls:
+            {
+                messages.Add(new AssistantChatMessage(completion));
+
+                foreach (ChatToolCall toolCall in completion.ToolCalls)
+                {
+                    switch (toolCall.FunctionName)
+                    {
+                        case nameof(ShowTables):
+                            {
+                                string toolResult = await ShowTables();
+                                messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                break;
+                            }
+
+                        case nameof(GetTableSchema):
+                            {
+                                using JsonDocument argumentsJson = JsonDocument.Parse(toolCall.FunctionArguments);
+                                bool hasTableName = argumentsJson.RootElement.TryGetProperty("tableName", out JsonElement tableName);
+
+                                if (!hasTableName)
+                                    throw new ArgumentNullException(nameof(tableName), "The tableName is required.");
+
+                                string toolResult = await GetTableSchema(tableName.GetString() ?? string.Empty);
+                                messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                break;
+                            }
+
+                        case nameof(RunDbCommand):
+                            {
+                                using JsonDocument argumentsJson = JsonDocument.Parse(toolCall.FunctionArguments);
+                                bool hasCommand = argumentsJson.RootElement.TryGetProperty("command", out JsonElement command);
+
+                                if (!hasCommand)
+                                    throw new ArgumentNullException(nameof(command), "The command is required.");
+
+                                string toolResult = await RunDbCommand(command.GetString() ?? string.Empty);
+                                messages.Add(new ToolChatMessage(toolCall.Id, toolResult));
+                                break;
+                            }
+
+                        default:
+                            {
+                                // Handle other unexpected calls.
+                                throw new NotImplementedException();
+                            }
+                    }
+                }
+
+                requiresAction = true;
+                break;
+            }
+
+        case ChatFinishReason.Length:
+            throw new NotImplementedException("Incomplete model output due to MaxTokens parameter or token limit exceeded.");
+
+        case ChatFinishReason.ContentFilter:
+            throw new NotImplementedException("Omitted content due to a content filter flag.");
+
+        case ChatFinishReason.FunctionCall:
+            throw new NotImplementedException("Deprecated in favor of tool calls.");
+
+        default:
+            throw new NotImplementedException(completion.FinishReason.ToString());
+    }
+} while (requiresAction);
+
+var finalResponse = messages.Last().Content;
+var reponseText = finalResponse[0].Text;
+Console.WriteLine(JsonSerializer.Serialize(finalResponse));
+
+var agentAnswerIndex = reponseText.IndexOf("ANSWER:", StringComparison.Ordinal);
+var rawAnswer = agentAnswerIndex != -1 
+    ? reponseText[(agentAnswerIndex + "ANSWER:".Length)..] 
+    : reponseText;
+
+var ids = rawAnswer
+    .Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+    .Select(int.Parse)
+    .ToImmutableArray();
+
+var taskResponse = new TaskAnswer<IList<int>>()
+{
+    Task = "database",
+    Answer = ids
+};
+var aiDevsApiService = sp.GetRequiredService<IAiDevsApiService>();
+var taskResponseResult = await aiDevsApiService.VerifyTaskAnswerAsync(taskResponse);
+Console.WriteLine(JsonSerializer.Serialize(taskResponseResult));
+
 async Task<string> ShowTables() => await RunDbCommand("show tables");
 
 async Task<string> GetTableSchema(string tableName) => await RunDbCommand($"show create table {tableName}");
@@ -77,4 +199,3 @@ async Task<string> RunDbCommand(string command)
 
     return await response.Content.ReadAsStringAsync();
 }
-
